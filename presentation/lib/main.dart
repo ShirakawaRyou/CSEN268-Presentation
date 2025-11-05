87e86a3168e84810ddd0cec286272aaa7dd44078
ShirakawaRyou <15baichuan@gmail.com>
2025-11-04 20:25:30 -0800

    更改为explicit animations

--- presentation/lib/main.dart
+++ presentation/lib/main.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'animated_container_card.dart';
-import 'animated_opacity_card.dart';
-import 'animated_padding_card.dart';
-import 'animated_align_card.dart';
-import 'animated_crossfade_card.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,82 +6,96 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Animation list',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Animation list'),
+      title: 'explicit animation',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const AnimationDemoPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class AnimationDemoPage extends StatefulWidget {
+  const AnimationDemoPage({super.key});
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<AnimationDemoPage> createState() => _AnimationDemoPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _AnimationDemoPageState extends State<AnimationDemoPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    _animation = Tween<double>(begin: 0, end: 200).animate(_controller)
+      ..addListener(() {
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Widget _buildAnimatedBox() {
+    return Container(
+      width: _animation.value,
+      height: _animation.value,
+      color: Colors.blueAccent,
+      alignment: Alignment.center,
+      child: const Text(
+        'animation',
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('explicit animation control'),
       ),
-      body: ListView(
-        padding: EdgeInsets.all(8),
-        children: const [
-          AnimatedContainerCard(),
-          AnimatedOpacityCard(),
-          AnimatedPaddingCard(),
-          AnimatedAlignCard(),
-          AnimatedCrossFadeCard(),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _buildAnimatedBox(),
+            const SizedBox(height: 40),
+            Wrap(
+              spacing: 12,
+              children: [
+                ElevatedButton(
+                  onPressed: () => _controller.forward(),
+                  child: const Text('start'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _controller.stop(),
+                  child: const Text('stop'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _controller.status == AnimationStatus.dismissed
+                      ? _controller.forward()
+                      : _controller.reverse(),
+                  child: const Text('reverse'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _controller.reset(),
+                  child: const Text('reset'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
